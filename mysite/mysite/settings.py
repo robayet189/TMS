25a44ea6265c4c6acee5f8c36d3065957e4bc89c
zakirhosainabir <zakirhossainabir7@gmail.com>
2026-05-16 04:43:44 +0600

    Use env-driven production settings and enable WhiteNoise

--- mysite/mysite/settings.py
+++ mysite/mysite/settings.py
@@ -9,20 +9,24 @@ import os
 BASE_DIR = Path(__file__).resolve().parent.parent
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-w0wtmb1*$w9*+olet#qff#t&42#bv-)8=a+7g%7-l=9&csu+rz'
+# Production should set `DJANGO_SECRET_KEY` in environment variables.
+SECRET_KEY = os.environ.get('DJANGO_SECRET_KEY', 'django-insecure-w0wtmb1*$w9*+olet#qff#t&42#bv-)8=a+7g%7-l=9&csu+rz')
 
-# SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+# DEBUG can be enabled by setting DJANGO_DEBUG=1 or True in the environment
+DEBUG = os.environ.get('DJANGO_DEBUG', 'False').lower() in ('1', 'true', 'yes')
 
-ALLOWED_HOSTS = ['*'] # Flexible for dev/staging
+# Configure ALLOWED_HOSTS from environment (comma-separated) or default to your PA domain
+ALLOWED_HOSTS = os.environ.get('ALLOWED_HOSTS', 'zakirhosainabir.pythonanywhere.com').split(',')
 
 # CSRF Settings for mobile app and hosting
 CSRF_TRUSTED_ORIGINS = [
     'https://*.pythonanywhere.com',
     'http://*.pythonanywhere.com',
 ]
-CSRF_COOKIE_SECURE = False  # Set to True in production with HTTPS
-CSRF_COOKIE_HTTPONLY = False
+# Use secure cookies in production when served over HTTPS
+CSRF_COOKIE_SECURE = os.environ.get('CSRF_COOKIE_SECURE', 'True').lower() in ('1', 'true', 'yes')
+CSRF_COOKIE_HTTPONLY = os.environ.get('CSRF_COOKIE_HTTPONLY', 'True').lower() in ('1', 'true', 'yes')
+SESSION_COOKIE_SECURE = os.environ.get('SESSION_COOKIE_SECURE', 'True').lower() in ('1', 'true', 'yes')
 
 # Application definition
 INSTALLED_APPS = [
@@ -41,6 +45,7 @@ INSTALLED_APPS = [
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
+    'whitenoise.middleware.WhiteNoiseMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
@@ -104,6 +109,9 @@ USE_TZ = True
 STATIC_URL = 'static/'
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static')]
+
+# Use WhiteNoise to serve static files efficiently in production
+STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 
 # Media files (Uploads)
 MEDIA_URL = '/media/'
